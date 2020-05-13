e1e5231f81d69e3a4c94f97b972f4ee82025caba
Jamie Evans <jamieevans96@googlemail.com>
2020-05-13 15:19:18 +0100

    Up and down time buttons added

--- script.js
+++ script.js
@@ -1,15 +1,39 @@
-var countdownTime = 1500000
+let countdownTime = 1500000;
+
+function upTime() {
+    countdownTime += 60000;
+
+    var minutes = Math.floor((countdownTime % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((countdownTime % (1000 * 60)) / 1000);
+
+    seconds = seconds.toString().padStart(2, '0');
+
+    document.getElementById("timer").innerHTML = minutes + ":" + seconds;
+}
+
+function downTime() {
+    countdownTime -= 60000;
+    
+    var minutes = Math.floor((countdownTime % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((countdownTime % (1000 * 60)) / 1000);
+
+    seconds = seconds.toString().padStart(2, '0');
+
+    document.getElementById("timer").innerHTML = minutes + ":" + seconds;
+}
 
 function myFunc() {
     var minutes = Math.floor((countdownTime % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((countdownTime % (1000 * 60)) / 1000);
 
-    document.getElementById("demo").innerHTML = minutes + "m " + seconds + "s ";
+    seconds = seconds.toString().padStart(2, '0');
+
+    document.getElementById("timer").innerHTML = minutes + ":" + seconds;
 
     countdownTime -= 1000;
 
     if (countdownTime < 0) {
         clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
+    document.getElementById("timer").innerHTML = "EXPIRED";
     }
 }
